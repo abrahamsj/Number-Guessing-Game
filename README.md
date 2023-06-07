08c99ec50066eeefb9c564694a8f2e7bcb1af6b9
Janeel Abrahams <abrahams.janeel@hotmail.com>
2023-06-07 09:51:10 -0400

    Intial game outline done in comments to start peer coding

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Number-Guessing-Game
+# Random-Number-Guessing-Game
+
+## Authors: Jin Hee Lee, Janeel Abrahams
+
+### The Random Number Guessing Game challenges players to guess a randomly selected number. The player is prompted to input their guess, and the program provides feedback on whether the guess is correct or incorrect.
+
+### This was developed using peer programming techniques.
---
+++ game.py
@@ -0,0 +1,22 @@
+"""Game Outline
+Goal fill in the code for the different steps/comments
+"""
+
+# import necessary libraries (this can be done during the coding process)
+
+
+
+#Display message that welcomes the user to the game and shows the rules of the game
+
+
+
+
+#Prompt the user for the range 
+# Get Max  number include input validation
+#Stretch Goal : get min and max number
+
+
+#prompt the user to input their guess for the number that was randomly selected.
+
+
+# display appropriate messages for user input. (higher,lower, or equal) to random number
